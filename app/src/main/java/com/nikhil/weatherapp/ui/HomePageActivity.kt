ed4041daf0083833ba3147d879fba6981d378a78
Nikhil <nikhilmht33@gmail.com>
2020-05-09 10:14:07 +0530

    Add Skeleton

--- app/src/main/java/com/nikhil/weatherapp/ui/HomePageActivity.kt
+++ app/src/main/java/com/nikhil/weatherapp/ui/HomePageActivity.kt
@@ -1,12 +1,13 @@
-package com.nikhil.weatherapp
+package com.nikhil.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nikhil.weatherapp.R
 
-class MainActivity : AppCompatActivity() {
+class HomePageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
     }
 }

970af4a290cc6c0396afd19664644482c5944c74
Nikhil <nikhilmht33@gmail.com>
2020-05-09 23:38:59 +0530

    Add Dagger Modules-Persistence and WeatherApi

--- app/src/main/java/com/nikhil/weatherapp/di/PersistenceModule.kt
+++ app/src/main/java/com/nikhil/weatherapp/di/PersistenceModule.kt
@@ -1,2 +1,19 @@
 package com.nikhil.weatherapp.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class PersistenceModule {
+
+    @Provides
+    @Singleton
+    fun providesSharedPreferences(context: Context):SharedPreferences{
+        return PreferenceManager.getDefaultSharedPreferences(context)
+    }
+
+}
--- app/src/main/java/com/nikhil/weatherapp/di/WeatherApiModule.kt
+++ app/src/main/java/com/nikhil/weatherapp/di/WeatherApiModule.kt
@@ -1,2 +1,65 @@
 package com.nikhil.weatherapp.di
 
+import com.nikhil.weatherapp.framework.network.service.WeatherApi
+import dagger.Module
+import dagger.Provides
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+class WeatherApiModule {
+
+    @Singleton
+    @Provides
+    fun restService(retrofit: Retrofit): WeatherApi =
+        retrofit.create(WeatherApi::class.java)
+
+    @Singleton
+    @Provides
+    fun retrofit(
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory,
+        rxJava2CallAdapterFactory: RxJava2CallAdapterFactory
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://example")
+            .client(okHttpClient)
+            .addConverterFactory(moshiConverterFactory)
+            .addCallAdapterFactory(rxJava2CallAdapterFactory)
+            .build()
+
+    @Provides
+    fun okHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .readTimeout(40, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    fun httpLoggingInterceptor() = HttpLoggingInterceptor {
+        Timber.tag("Network Call").d(it)
+    }.apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+
+    @Provides
+    fun moshiConverterFactory(): Converter.Factory = MoshiConverterFactory.create()
+
+    @Provides
+    fun rxJava2CallAdapterFactory(): RxJava2CallAdapterFactory =
+        RxJava2CallAdapterFactory.createWithScheduler(
+            Schedulers.io()
+        )
+}
